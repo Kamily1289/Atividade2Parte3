2f71368dbb67c19f406b300b24c1ce50b774fe1e
Kamily1289 <kamilyguedesmariakamilyguedes@gmail.com>
2025-11-14 22:40:33 -0300

    Parse query string into JSON object

--- Questão5.js
+++ Questão5.js
@@ -1,1 +1,6 @@
+const queryString = "categoria=eletronicos&preco=500&marca=samsung&avaliacao=4.5";
 
+const params = new URLSearchParams(queryString);
+const jsonObj = Object.fromEntries(params);
+
+console.log(jsonObj);
